e715d623eee39a67c1a05fb767552a9fe83c1e33
lexli8888 <alexander.van.schie@hsr.ch>
2017-12-14 10:29:08 +0100

    ServiceContracts

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,25 +1,43 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
 using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract]
     public interface IAutoReservationService
     {
-         List<AutoDto> getAllCars();
+        [OperationContract]
+        List<AutoDto> getAllCars();
+        [OperationContract]
         List<KundeDto> getAllCustomers();
+        [OperationContract]
         List<ReservationDto> getAllReservations();
+        [OperationContract]
         AutoDto getCarById(int id);
+        [OperationContract]
         KundeDto getCustomerById(int id);
+        [OperationContract]
         ReservationDto getReservationByNr(int nr);
+        [OperationContract]
         void addCar(AutoDto autoDto);
+        [OperationContract]
         void addCustomer(KundeDto kundeDto);
+        [OperationContract]
         void addRerservation(ReservationDto reservationDto);
+        [OperationContract]
         void modifyCar(AutoDto autoDto);
+        [OperationContract]
         void modifyCustomer(KundeDto kundeDto);
+        [OperationContract]
         void modifyRerservation(ReservationDto reservationDto);
+        [OperationContract]
         void removeCar(AutoDto autoDto);
+        [OperationContract]
         void removeCustomer(KundeDto kundeDto);
+        [OperationContract]
         void removeRerservation(ReservationDto reservationDto);
+        [OperationContract]
         bool isCarAvailable(ReservationDto reservation, AutoDto auto);
 
     }
